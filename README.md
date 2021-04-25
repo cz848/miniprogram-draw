949353e855854481c3aebf5f93e1a1dcf6c7d4b2
Tyler Chao <tyler.chao@verystar.cn>
2021-04-25 15:57:01 +0800

    feat: 更换为创建class方式

--- README.md
+++ README.md
@@ -4,9 +4,20 @@
 
 ## 安装
 
-```javascript
+```bash
 npm install miniprogram-draw
 ```
+
+## 使用
+
+创建绘图实例
+
+```javascript
+// 获取canvas上下文
+const draw = new Draw(canvasId: String, canvasWidth: Number, canvasHeight: Number, ratio?: Number);
+```
+
+其中，`ratio`可省略，默认为当前机型的devicePixelRatio
 
 ## APIs
 
@@ -28,55 +39,63 @@ draw.utils.rpx2px(20);
 draw.utils.px2rpx(20);
 ```
 
-#### 载入网络图片 `loadImages(input: Array): Array`
-
-```javascript
-draw.utils.loadImages(['https://xx', 'https://xx'])
-```
-
-#### 设置css font属性 `font(canvas: Object, value: Number | String)`
+#### 设置css font属性 `font(value: Number | String)`
 
 ```javascript
 draw.utils.font(20)
 draw.utils.font('bold 30rpx')
 ```
 
+#### 载入网络图片 `loadImages(input: Array): Array`
+
+```javascript
+Draw.loadImages(['https://xx', 'https://xx'])
+```
+
 ### 图像
 
-#### 绘制图片 `image(canvas: Object, src: String, x: Number, y: Number, width: Number, height: Number)`
+#### 绘制图片 `image(src: String, x: Number, y: Number, width: Number, height: Number)`
 
-#### 居中绘制一张图片 `centerImage(canvas: Object, src: String, x: Number, y: Number, width: Number, height: Number, cWidth: Number)`
+```javascript
+draw.image('https://xx', 10, 20, 120, 100);
+```
+
+调用示例下同
+
+#### 居中绘制一张图片 `centerImage(src: String, x: Number, y: Number, width: Number, height: Number, cWidth?: Number)`
 
 ### 图形
 
-#### 绘制矩形 `rect(canvas: Object, x: Number, y: Number, width: Number, height: Number, color: String, border: String, shadow: String, padding: Number)`
+#### 绘制矩形 `rect(x: Number, y: Number, width: Number, height: Number, color?: String, border?: String, shadow?: String, padding?: Number)`
 
-#### 绘制圆角矩形 `roundRect(canvas: Object, x: Number, y: Number, width: Number, height: Number, radius: Number, color: String, border: String, shadow: String, padding: Number)`
+#### 绘制圆角矩形 `roundRect(x: Number, y: Number, width: Number, height: Number, radius: Number, color?: String, border?: String, shadow?: String, padding?: Number)`
 
-#### 绘制圆形　`circle(canvas: Object, x: Number, y: Number, diameter: Number, color: String, border: String, shadow: String, padding: Number)`
+#### 绘制圆形　`circle(x: Number, y: Number, diameter: Number, color?: String, border?: String, shadow?: String, padding?: Number)`
 
-#### 绘制椭圆 `ellipse(canvas: Object, x: Number, y: Number, width: Number, height: Number, color: String, border: String, shadow: String, padding: Number)`
+#### 绘制椭圆 `ellipse(x: Number, y: Number, width: Number, height: Number, color?: String, border?: String, shadow?: String, padding?: Number)`
 
-#### 绘制填充图像的矩形 `rectImage(canvas: Object, src: String, x: Number, y: Number, width: Number, height: Number, border: String, shadow: String, padding: Number, color: String)`
+#### 绘制填充图像的矩形 `rectImage(src: String, x: Number, y: Number, width: Number, height: Number, border?: String, shadow?: String, padding?: Number, color?: String)`
 
-#### 绘制填充图像的圆角矩形 `roundRectImage(canvas: Object, src: String, x: Number, y: Number, width: Number, height: Number, radius: Number, border: String, shadow: String, padding: Number, color: String)`
+#### 绘制填充图像的圆角矩形 `roundRectImage(src: String, x: Number, y: Number, width: Number, height: Number, radius: Number, border?: String, shadow?: String, padding?: Number, color?: String)`
 
-#### 绘制填充图像的圆形 `circleImage(canvas: Object, src: String, x: Number, y: Number, diameter: Number, border: String, shadow: String, padding: Number, color: String)`
+#### 绘制填充图像的圆形 `circleImage(src: String, x: Number, y: Number, diameter: Number, border?: String, shadow?: String, padding?: Number, color?: String)`
 
-#### 绘制填充图像的椭圆形 `ellipseImage(canvas: Object, src: String, x: Number, y: Number, width: Number, height: Number, border: String, shadow: String, padding: Number, color: String)`
+#### 绘制填充图像的椭圆形 `ellipseImage(src: String, x: Number, y: Number, width: Number, height: Number, border?: String, shadow?: String, padding?: Number, color?: String)`
 
 ### 文本
 
-#### 绘制单行文本 `text(canvas: Object, txt: String, x: Number, y: Number, font: String | Number, color: String, stroke: String | Number, maxWidth: Number)`
+#### 绘制单行文本 `text(txt: String, x: Number, y: Number, font: String | Number, color?: String, stroke?: String | Number, maxWidth?: Number)`
 
-#### 居中绘制一行文本 `centerText(canvas: Object, txt: String, x: Number, y: Number, width: Number, font: String | Number, color: String, stroke: String | Number)`
+#### 居中绘制一行文本 `centerText(txt: String, x: Number, y: Number, width: Number, font: String | Number, color?: String, stroke?: String | Number)`
 
-#### 绘制多行文本　`paragraph(canvas: Object, txt: String, x: Number, y: Number, maxWidth: Number, lineHeigh: Number, maxLine: Number, font: String | Number, color: String, stroke: String | Number)`
+#### 绘制多行文本　`paragraph(txt: String, x: Number, y: Number, maxWidth: Number, lineHeigh: Number, maxLine: Number, font: String | Number, color?: String, stroke?: String | Number)`
 
 ### 生成
 
-#### 绘制到画布上 `toCanvas(canvas: Object, reverse: Boolean)`
+#### 绘制到画布上 `toCanvas(reverse?: Boolean)`
 返回Promise
 
-#### 输出为图片 `toImage(canvas: Object, canvasId: String, options: Object)`
+#### 输出为图片 `toImage(options: Object)`
 返回Promise
+
+options 参数同`canvasToTempFilePath`参数
--- index.js
+++ index.js
@@ -8,34 +8,34 @@ import { mp, getSystemInfo } from 'miniprogram-tools/weapp';
 
 mp.add(['getImageInfo', 'canvasToTempFilePath']);
 
-const { pxRatio } = getSystemInfo();
-
-// 为了图片清晰及计算方便，这里统一为2倍大小
-const rpx2px = value => (Number(value) ? value * pxRatio * 2 : value);
-
-const px2rpx = value => value / pxRatio / 2;
-
-/*
- * 以下传参时全用rpx单位，并内部转换为px
- */
-
-// 转换传入字体为标准的css格式，字号只能用rpx单位
-const font = (value = '') => {
-  if (typeof value === 'number') return `${rpx2px(value).toFixed(0)}px sans-serif`;
-  let fontFamily = '';
-  if (!String(value).match(/ +((\b|'|")[a-z][\w- ]+\1);?$/gi)) fontFamily = ' sans-serif';
-  return String(value)
-    .replace(/;$/, '')
-    .replace(/\b(\d+)rpx\b/gi, ($0, $1) => `${rpx2px($1).toFixed(0)}px`)
-    + fontFamily;
-};
-
 // 从网络地址获取图片信息
 // pics: 图片地址列表
 // 使用getImageInfo逐个下载图像，并把网络地址替换为本地路径
 const loadImages = (pics = []) => Promise.all(pics.map(src => (src ? mp.getImageInfo({ src }) : '')))
   .then(res => res.map((item, i) => (String(pics[i]).match(/^https?:/) ? item.path : pics[i])))
   .catch(e => e);
+
+const { pxRatio = 0.5, devicePixelRatio = 2 } = getSystemInfo();
+
+// 为了图片清晰，这里设定ratio为放大倍数
+const rpx2px = (value, ratio = devicePixelRatio) => (Number(value) ? value * pxRatio * ratio : value);
+
+const px2rpx = (value, ratio = devicePixelRatio) => value / pxRatio / ratio;
+
+/*
+ * 以下传参时全用rpx单位，并内部转换为px
+ */
+
+// 转换传入字体为标准的css格式，字号只能用rpx单位
+const font = (value = '', ratio = devicePixelRatio) => {
+  if (typeof value === 'number') return `${rpx2px(value, ratio).toFixed(0)}px sans-serif`;
+  let fontFamily = '';
+  if (!String(value).match(/ +((\b|'|")[a-z][\w- ]+\1);?$/gi)) fontFamily = ' sans-serif';
+  return String(value)
+    .replace(/;$/, '')
+    .replace(/\b(\d+)rpx\b/gi, ($0, $1) => `${rpx2px($1, ratio).toFixed(0)}px`)
+    + fontFamily;
+};
 
 /* ======== 内部方法 ========== */
 const isColor = v => /^(?:#|rgba?|transparent)/i.test(v);
@@ -44,9 +44,9 @@ const isNumber = v => !Number.isNaN(+v);
 const isString = v => typeof v === 'string';
 const isStyle = v => /^solid|dashed$/.test(v);
 
-// 把边框解析为数组[1, 'solid', '#000']
-const border = value => {
-  const bd = [1, 'solid', '#000'];
+// 把边框解析为数组[0, 'solid', '#000']，style目前只支持solid和dashed
+const border = (that, value) => {
+  const bd = [0, 'solid', '#000'];
 
   if (isNumber(value)) return [value].concat(bd.slice(1, 3));
   if (isString(value)) {
@@ -56,7 +56,7 @@ const border = value => {
       else if (isColor(v)) acc[2] = v;
       else {
         v = unitless(v);
-        if (isNumber(v)) acc[0] = rpx2px(v);
+        if (isNumber(v)) acc[0] = that.utils.rpx2px(v);
       }
       return acc;
     }, bd);
@@ -89,7 +89,7 @@ const dashed = (ctx, w) => {
 /*
  * 在图形中填充图像
  *
- * src: [图像网络地址]
+ * src:[图像网络地址]
  * x:  [要填充的起始x坐标]
  * y:  [要填充的起始y坐标]
  * w:  [要填充的宽度]
@@ -103,23 +103,26 @@ const fillImage = (ctx, src, x, y, w, h, p) => {
     w -= p * 2;
     h -= p * 2;
   }
+  ctx.save();
   ctx.clip();
   ctx.drawImage(src, x, y, w, h);
+  ctx.restore();
 };
 
 /*
- * 把路径绘制到画布上
- * c:   [矩形填充色]
- * bd:  [矩形边框样式]
- * shd: [矩形阴影样式]
- * x:  [矩形起始x坐标]
- * y:  [矩形起始y坐标]
- * w:  [矩形宽度]
- * h:  [矩形高度]
- * src: [图像网络地址]
- * p:  [要填充图像的内边距]
+ * 把形状绘制到画布上
+ * c:  [形状填充色]
+ * bd: [形状边框样式]
+ * shd:[形状阴影样式]
+ * x:  [形状起始x坐标]
+ * y:  [形状起始y坐标]
+ * w:  [形状宽度]
+ * h:  [形状高度]
+ * src:[填充图像的地址]
+ * p:  [填充图像的内边距]
  */
-const shape = (ctx, c, bd, shd, x, y, w, h, src, p) => {
+const shape = (that, c, bd, shd, x, y, w, h, src, p) => {
+  const { ctx } = that;
   // 设定shadow参数
   ctx.save();
 
@@ -133,7 +136,7 @@ const shape = (ctx, c, bd, shd, x, y, w, h, src, p) => {
 
   if (bd) {
     // 设定边框线参数
-    const [bw, bs, bc] = border(bd);
+    const [bw, bs, bc] = border(that, bd);
     ctx.lineWidth = bw;
     ctx.strokeStyle = bc;
     if (bs === 'dashed') dashed(ctx, bw);
@@ -168,326 +171,343 @@ const shape = (ctx, c, bd, shd, x, y, w, h, src, p) => {
   ctx.restore();
 };
 
-// 依次用rpx2px转换
-const bcr = (...args) => args.map(rpx2px);
+const bcr = (that, ...args) => args.map(that.utils.rpx2px);
 
 /* /==================== */
 
-/**
- * 绘制一张图片
- *
- * src: [要绘制的图片地址]
- * x:   [起始x坐标]
- * y:   [起始y坐标]
- * w:   [图片宽度]
- * h:   [图片高度]
- */
-const image = (ctx, src, x, y, w, h) => {
-  ctx.drawImage(src, ...bcr(x, y, w, h));
-};
+class Draw {
+  constructor(canvasId, w, h, ratio = devicePixelRatio) {
+    this.init(canvasId, w, h, ratio);
+  }
 
-/*
- * 绘制矩形
- *
- * x:   [矩形起始x坐标]
- * y:   [矩形起始y坐标]
- * w:   [矩形宽度]
- * h:   [矩形高度]
- * c:   [矩形填充色]
- * bd:  [矩形边框样式]
- * shd: [矩形阴影样式]
- * src: [填充图像的地址]
- */
-const rect = (ctx, x, y, w, h, c, bd, shd, src, p) => {
-  [x, y, w, h, p] = bcr(x, y, w, h, p);
-  // 创建一个矩形路径
-  ctx.rect(x, y, w, h);
+  init(canvasId, w, h, ratio) {
+    this.ctx = wx.createCanvasContext(canvasId);
+    this.canvasId = canvasId;
+    this.width = w;
+    this.height = h;
+    this.ratio = ratio;
+  }
 
-  // 根据填充色、边框、阴影画出此路径
-  shape(ctx, c, bd, shd, x, y, w, h, src, p);
-};
+  utils = {
+    rpx2px: value => rpx2px(value, this.ratio),
+    px2rpx: value => px2rpx(value, this.ratio),
+    font: value => {
+      this.ctx.font = font(value, this.ratio);
+    },
+  }
 
-/**
- * 绘制圆角矩形
- *
- * x: [起始x坐标]
- * y: [起始y坐标]
- * w: [宽度]
- * h: [高度]
- * r: [圆角半径，可传入数组，以支持单个圆角定义]
- * c: [填充色]
- * bd: [边框样式]
- * shd: [阴影样式]
- * src: [填充图片的地址]
- */
-const roundRect = (ctx, x, y, w, h, r, c, bd, shd, src, p) => {
-  [x, y, w, h, p] = bcr(x, y, w, h, p);
+  /**
+   * 绘制一张图片
+   *
+   * src: [要绘制的图片地址]
+   * x:   [起始x坐标]
+   * y:   [起始y坐标]
+   * w:   [图片宽度]
+   * h:   [图片高度]
+   */
+  image(src, x, y, w, h) {
+    this.ctx.drawImage(src, ...bcr(this, x, y, w, h));
+  }
 
-  let drawing = true;
+  /*
+   * 绘制矩形
+   *
+   * x:   [矩形起始x坐标]
+   * y:   [矩形起始y坐标]
+   * w:   [矩形宽度]
+   * h:   [矩形高度]
+   * c:   [矩形填充色]
+   * bd:  [矩形边框样式]
+   * shd: [矩形阴影样式]
+   * src: [填充图像的地址]
+   */
+  rect(x, y, w, h, c, bd, shd, src, p) {
+    [x, y, w, h, p] = bcr(this, x, y, w, h, p);
 
-  if (drawing) {
-    // 开始绘制
+    // 创建一个矩形路径
+    this.ctx.rect(x, y, w, h);
+
+    // 根据填充色、边框、阴影画出此路径
+    shape(this, c, bd, shd, x, y, w, h, src, p);
+  }
+
+  /**
+   * 绘制圆角矩形
+   *
+   * x:  [起始x坐标]
+   * y:  [起始y坐标]
+   * w:  [宽度]
+   * h:  [高度]
+   * r:  [圆角半径，可传入数组，以支持单个圆角定义]
+   * c:  [填充色]
+   * bd: [边框样式]
+   * shd:[阴影样式]
+   * src:[填充图片的地址]
+   */
+  roundRect(x, y, w, h, r, c, bd, shd, src, p) {
+    [x, y, w, h, p] = bcr(this, x, y, w, h, p);
+
+    const { ctx } = this;
+    let drawing = true;
+
+    if (drawing) {
+      // 开始绘制
+      ctx.beginPath();
+      // 分别从左上，右上，右下，左下绘制四个圆角
+      // r的单位转换放到此处一起处理，少一次判断
+      if (!Array.isArray(r)) r = [r, r, r, r];
+      const [rTL = 0, rTR = 0, rBR = 0, rBL = 0] = Array.from(r, this.utils.rpx2px);
+      ctx.arc(x + rTL, y + rTL, rTL, Math.PI, Math.PI * 1.5);
+      ctx.arc(x + w - rTR, y + rTR, rTR, Math.PI * 1.5, Math.PI * 2);
+      ctx.arc(x + w - rBR, y + h - rBR, rBR, 0, Math.PI * 0.5);
+      ctx.arc(x + rBL, y + h - rBL, rBL, Math.PI * 0.5, Math.PI);
+      ctx.closePath();
+
+      shape(this, c, bd, shd, x, y, w, h, src, p);
+    }
+    drawing = false;
+  }
+
+  /**
+   * 绘制圆形
+   *
+   * x: [圆形x坐标]
+   * y: [圆形y坐标]
+   * d: [圆形直径]
+   * c: [圆形填充色]
+   * bd: [圆形边框样式]
+   * shd: [圆形阴影样式]
+   * src: [填充图片的地址]
+   */
+  circle(x, y, d, c, bd, shd, src, p) {
+    this.roundRect(x, y, d, d, d / 2, c, bd, shd, src, p);
+  }
+
+  /**
+   * 绘制椭圆
+   *
+   * x: [椭圆x坐标]
+   * y: [椭圆y坐标]
+   * w: [椭圆轴1]
+   * h: [椭圆轴2]
+   * c: [椭圆填充色]
+   * bd: [椭圆边框样式]
+   * shd: [椭圆阴影样式]
+   * src: [填充图片的地址]
+   */
+  ellipse(x, y, w, h, c, bd, shd, src, p) {
+    [x, y, w, h, p] = bcr(this, x, y, w, h, p);
+    const { ctx } = this;
+    // 以圆心为中点画椭圆
+    // 椭圆的参数方程：x=acosθ | y=bsinθ，这里x,y为左上角图标，所以相对圆心需要再加上a,b。
+    const a = w / 2;
+    const b = h / 2;
+    const step = (a > b) ? 1 / a : 1 / b;
     ctx.beginPath();
-    // 分别从左上，右上，右下，左下绘制四个圆角
-    // r的单位转换放到此处一起处理，少一次判断
-    if (!Array.isArray(r)) r = [r, r, r, r];
-    const [rTL = 0, rTR = 0, rBR = 0, rBL = 0] = Array.from(r, rpx2px);
-    ctx.arc(x + rTL, y + rTL, rTL, Math.PI, Math.PI * 1.5);
-    ctx.arc(x + w - rTR, y + rTR, rTR, Math.PI * 1.5, Math.PI * 2);
-    ctx.arc(x + w - rBR, y + h - rBR, rBR, 0, Math.PI * 0.5);
-    ctx.arc(x + rBL, y + h - rBL, rBL, Math.PI * 0.5, Math.PI);
+    ctx.moveTo(x + 2 * a, y + b);
+    for (let i = 0; i < 2 * Math.PI; i += step) {
+      ctx.lineTo(x + a * (1 + Math.cos(i)), y + b * (1 + Math.sin(i)));
+    }
     ctx.closePath();
 
-    shape(ctx, c, bd, shd, x, y, w, h, src, p);
+    // 绘制到画布上
+    shape(this, c, bd, shd, x, y, w, h, src, p);
   }
-  drawing = false;
-};
 
-/**
- * 绘制圆形
- *
- * x: [圆形x坐标]
- * y: [圆形y坐标]
- * d: [圆形直径]
- * c: [圆形填充色]
- * bd: [圆形边框样式]
- * shd: [圆形阴影样式]
- * src: [填充图片的地址]
- */
-const circle = (ctx, x, y, d, c, bd, shd, src, p) => {
-  roundRect(ctx, x, y, d, d, d / 2, c, bd, shd, src, p);
-};
-
-/**
- * 绘制椭圆
- *
- * x: [椭圆x坐标]
- * y: [椭圆y坐标]
- * w: [椭圆轴1]
- * h: [椭圆轴2]
- * c: [椭圆填充色]
- * bd: [椭圆边框样式]
- * shd: [椭圆阴影样式]
- * src: [填充图片的地址]
- */
-const ellipse = (ctx, x, y, w, h, c, bd, shd, src, p) => {
-  [x, y, w, h, p] = bcr(x, y, w, h, p);
-
-  // 以圆心为中点画椭圆
-  // 椭圆的参数方程：x=acosθ | y=bsinθ，这里x,y为左上角图标，所以相对圆心需要再加上a,b。
-  const a = w / 2;
-  const b = h / 2;
-  const step = (a > b) ? 1 / a : 1 / b;
-  ctx.beginPath();
-  ctx.moveTo(x + 2 * a, y + b);
-  for (let i = 0; i < 2 * Math.PI; i += step) {
-    ctx.lineTo(x + a * (1 + Math.cos(i)), y + b * (1 + Math.sin(i)));
+  /**
+   * 绘制填充图像的矩形
+   */
+  rectImage(src, x, y, w, h, bd, shd, p, c) {
+    this.rect(x, y, w, h, c, bd, shd, src, p);
   }
-  ctx.closePath();
 
-  // 绘制到画布上
-  shape(ctx, c, bd, shd, x, y, w, h, src, p);
-};
-
-/**
- * 绘制填充图像的矩形
- */
-const rectImage = (ctx, src, x, y, w, h, bd, shd, p, c) => {
-  rect(ctx, x, y, w, h, c, bd, shd, src, p);
-};
-
-/**
- * 绘制填充图像的圆角矩形
- */
-const roundRectImage = (ctx, src, x, y, w, h, r, bd, shd, p, c) => {
-  roundRect(ctx, x, y, w, h, r, c, bd, shd, src, p);
-};
-
-/**
- * 绘制填充图像的圆形
- */
-const circleImage = (ctx, src, x, y, d, bd, shd, p, c) => {
-  circle(ctx, x, y, d, c, bd, shd, src, p);
-};
-
-/**
- * 绘制填充图像的椭圆形
- */
-const ellipseImage = (ctx, src, x, y, w, h, bd, shd, p, c) => {
-  ellipse(ctx, x, y, w, h, c, bd, shd, src, p);
-};
-
-/**
- * 居中绘制一张图片
- *
- * src: [要绘制的图片地址]
- * x:   [起始x坐标]
- * y:   [起始y坐标]
- * w:   [图片宽度]
- * h:   [图片高度]
- * cw:  [要居中区域的宽度]
- */
-const centerImage = (ctx, src, x, y, w, h, cw) => {
-  [x, y, w, h, cw] = bcr(x, y, w, h, cw);
-
-  x += (cw - w) / 2;
-
-  ctx.drawImage(src, x, y, w, h);
-};
-
-/**
- * 绘制单行文本
- *
- * txt: [要绘制的一行文本]
- * x:   [x坐标]
- * y:   [y坐标]
- * fs:  [字体]
- * c:   [字体颜色]
- * sk:  [字体描边]
- * w:   [需要绘制的最大宽度]
- */
-
-const text = (ctx, txt, x, y, fs, c, sk, w) => {
-  [x, y, w] = bcr(x, y, w);
-  ctx.font = font(fs);
-
-  if (c) ctx.fillStyle = c;
-  ctx.save();
-  ctx.setTextBaseline('top');
-  if (sk) {
-    const [skw, , skc] = border(sk);
-    ctx.lineWidth = skw;
-    ctx.strokeStyle = skc;
-    ctx.strokeText(txt, x, y, w);
+  /**
+   * 绘制填充图像的圆角矩形
+   */
+  roundRectImage(src, x, y, w, h, r, bd, shd, p, c) {
+    this.roundRect(x, y, w, h, r, c, bd, shd, src, p);
   }
-  ctx.fillText(txt, x, y, w);
-  ctx.restore();
-};
 
-/**
- * 居中绘制一行文本
- *
- * txt: [要绘制的一行文本]
- * x:   [起始x坐标]
- * y:   [起始y坐标]
- * w:   [整体宽度]
- * fs:  [字体]
- * c:   [字体颜色]
- * sk:  [字体描边]
- */
-
-const centerText = (ctx, txt, x, y, w, fs, c, sk) => {
-  [x, y, w] = bcr(x, y, w);
-  ctx.font = font(fs);
-
-  if (c) ctx.fillStyle = c;
-  x += w / 2;
-
-  ctx.save();
-  ctx.setTextBaseline('top');
-  ctx.setTextAlign('center');
-  if (sk) {
-    const [skw, , skc] = border(sk);
-    ctx.lineWidth = skw;
-    ctx.strokeStyle = skc;
-    ctx.strokeText(txt, x, y, w);
+  /**
+   * 绘制填充图像的圆形
+   */
+  circleImage(src, x, y, d, bd, shd, p, c) {
+    this.circle(x, y, d, c, bd, shd, src, p);
   }
-  ctx.fillText(txt, x, y, w);
-  ctx.restore();
-};
 
-/**
- * 绘制多行文本
- *
- * txt: [要绘制的大段文本]
- * x:   [起始x坐标]
- * y:   [起始y坐标]
- * w:   [一行宽度]
- * lh:  [行高]
- * ln:  [最大行数]
- * fs:  [字体]
- * c:   [字体颜色]
- * sk:  [字体描边]
- */
-const paragraph = (ctx, txt, x, y, w, lh, ln, fs, c, sk) => {
-  [x, y, w, lh] = bcr(x, y, w, lh);
-  ctx.font = font(fs);
+  /**
+   * 绘制填充图像的椭圆形
+   */
+  ellipseImage(src, x, y, w, h, bd, shd, p, c) {
+    this.ellipse(x, y, w, h, c, bd, shd, src, p);
+  }
 
-  // 文本宽度
-  const tw = ctx.measureText(txt).width;
-  const textLines = [''];
-  let i = 0;
-  if (tw > w) {
-    // 最大行宽
-    let lw = 0;
-    txt.split('').forEach(t => {
-      textLines[i] += t;
-      lw = ctx.measureText(textLines[i]).width;
-      if (lw >= w) {
-        // 本行退回一个字符
-        textLines[i] = textLines[i].slice(0, -1);
-        i += 1;
-        // 并丢到下一行开始
-        textLines[i] = t;
+  /**
+   * 居中绘制一张图片
+   *
+   * src: [要绘制的图片地址]
+   * x:   [起始x坐标]
+   * y:   [起始y坐标]
+   * w:   [图片宽度]
+   * h:   [图片高度]
+   * cw:  [要居中区域的宽度]
+   */
+  centerImage(src, x, y, w, h, cw) {
+    [x, y, w, h, cw] = bcr(this, x, y, w, h, cw || this.width);
+    x += (cw - w) / 2;
+
+    this.ctx.drawImage(src, x, y, w, h);
+  }
+
+  centerImageToCanvas(src, y, w, h) {
+    this.centerImage(src, 0, y, w, h);
+  }
+
+  /**
+   * 绘制单行文本
+   *
+   * txt: [要绘制的一行文本]
+   * x:   [x坐标]
+   * y:   [y坐标]
+   * fs:  [字体]
+   * c:   [字体颜色]
+   * sk:  [字体描边]
+   * w:   [需要绘制的最大宽度]
+   */
+  text(txt, x, y, fs, c, sk, w) {
+    [x, y, w] = bcr(this, x, y, w || this.width);
+
+    const { ctx } = this;
+
+    ctx.save();
+    if (fs) this.utils.font(fs);
+    if (c) ctx.fillStyle = c;
+    ctx.setTextBaseline('top');
+    if (sk) {
+      const [skw, , skc] = border(this, sk);
+      ctx.lineWidth = skw;
+      ctx.strokeStyle = skc;
+      ctx.strokeText(txt, x, y, w);
+    }
+    ctx.fillText(txt, x, y, w);
+    ctx.restore();
+  }
+
+  /**
+   * 居中绘制一行文本
+   *
+   * txt: [要绘制的一行文本]
+   * x:   [起始x坐标]
+   * y:   [起始y坐标]
+   * w:   [整体宽度]
+   * fs:  [字体]
+   * c:   [字体颜色]
+   * sk:  [字体描边]
+   */
+  centerText(txt, x, y, w, fs, c, sk) {
+    [x, y, w] = bcr(this, x, y, w || this.width);
+    x += w / 2;
+
+    const { ctx } = this;
+
+    ctx.save();
+    if (fs) this.utils.font(fs);
+    if (c) ctx.fillStyle = c;
+
+    ctx.setTextBaseline('top');
+    ctx.setTextAlign('center');
+    if (sk) {
+      const [skw, , skc] = border(this, sk);
+      ctx.lineWidth = skw;
+      ctx.strokeStyle = skc;
+      ctx.strokeText(txt, x, y, w);
+    }
+    ctx.fillText(txt, x, y, w);
+    ctx.restore();
+  }
+
+  // 居中于画布绘制文本
+  centerTextToCanvas(txt, y, fs, c, sk) {
+    this.centerText(txt, 0, y, null, fs, c, sk);
+  }
+
+  /**
+   * 绘制多行文本
+   *
+   * txt: [要绘制的大段文本]
+   * x:   [起始x坐标]
+   * y:   [起始y坐标]
+   * w:   [一行宽度]
+   * lh:  [行高]
+   * ln:  [最大行数]
+   * fs:  [字体]
+   * c:   [字体颜色]
+   * sk:  [字体描边]
+   */
+  paragraph(txt, x, y, w, lh, ln, fs, c, sk) {
+    [x, y, w, lh] = bcr(this, x, y, w, lh);
+    const { ctx } = this;
+
+    ctx.save();
+    if (fs) this.utils.font(fs);
+    if (c) ctx.fillStyle = c;
+    // 文本宽度
+    const tw = ctx.measureText(txt).width;
+    const textLines = [''];
+    let i = 0;
+    if (tw > w) {
+      // 最大行宽
+      let lw = 0;
+      txt.split('').forEach(t => {
+        textLines[i] += t;
+        lw = ctx.measureText(textLines[i]).width;
+        if (lw >= w) {
+          // 本行退回一个字符
+          textLines[i] = textLines[i].slice(0, -1);
+          i += 1;
+          // 并丢到下一行开始
+          textLines[i] = t;
+        }
+      });
+    } else {
+      textLines[0] = txt;
+    }
+
+    ctx.setTextBaseline('top');
+    if (sk) {
+      const [skw, , skc] = border(this, sk);
+      ctx.lineWidth = skw;
+      ctx.strokeStyle = skc;
+    }
+    textLines.forEach((line, j) => {
+      if (j === ln - 1 && i > ln) line = `${line.slice(0, line.length - 1)}...`;
+      if (j < ln) {
+        ctx.strokeText(line, x, y + lh * (j + 1));
+        ctx.fillText(line, x, y + lh * (j + 1));
       }
     });
-  } else {
-    textLines[0] = txt;
+    ctx.restore();
   }
 
-  if (c) ctx.fillStyle = c;
-  ctx.save();
-  ctx.setTextBaseline('top');
-  if (sk) {
-    const [skw, , skc] = border(sk);
-    ctx.lineWidth = skw;
-    ctx.strokeStyle = skc;
+  /*
+   * 绘制到画布上
+   */
+  toCanvas(reverse) {
+    return new Promise(resolve => this.ctx.draw(reverse, resolve));
   }
-  textLines.forEach((line, j) => {
-    if (j === ln - 1 && i > ln) line = `${line.slice(0, line.length - 1)}...`;
-    if (j < ln) {
-      ctx.strokeText(line, x, y + lh * (j + 1));
-      ctx.fillText(line, x, y + lh * (j + 1));
-    }
-  });
-  ctx.restore();
-};
 
-/*
- * 绘制到画布上
- */
-const toCanvas = (ctx, reverse) => new Promise(resolve => ctx.draw(reverse, resolve));
+  /* 输入画布内容为指定大小的图片，得到文件的临时本地路径
+   * options:  [传入canvasToTempFilePath的其它参数]
+   * @return path: 文件的临时路径
+   */
+  toImage(options) {
+    return this.toCanvas()
+      .then(() => mp.canvasToTempFilePath({ canvasId: this.canvasId, ...options }))
+      .then(res => ({ path: res.tempFilePath }))
+      .catch(e => e);
+  }
+}
 
-/* 输入画布内容为指定大小的图片，得到文件的临时本地路径
- * canvasId: [画布标识，传入 canvas 组件的 canvas-id]
- * options:  [传入canvasToTempFilePath的其它参数]
- * @return path: 文件的临时路径
- */
-const toImage = (ctx, canvasId, options) => toCanvas(ctx)
-  .then(() => mp.canvasToTempFilePath({ canvasId, ...options }))
-  .then(res => ({ path: res.tempFilePath }))
-  .catch(e => e);
+Draw.loadImages = loadImages;
 
-export default {
-  utils: {
-    rpx2px,
-    px2rpx,
-    font,
-    loadImages,
-  },
-  image,
-  rect,
-  rectImage,
-  roundRect,
-  roundRectImage,
-  circle,
-  circleImage,
-  ellipse,
-  ellipseImage,
-  text,
-  paragraph,
-  centerText,
-  centerImage,
-  toCanvas,
-  toImage,
-};
+export default Draw;
